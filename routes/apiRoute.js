cac954107fe398f80df3f3e7b83c1b580958878d
Tonya Fletcher <tfletch4504@gmail.com>
2021-04-04 12:31:02 -0400

    apiroute

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,18 +1,62 @@
-const note_data = require("../db/note_data");
+const fs = require("fs");
+const { parse } = require("path");
+
 module.exports = function(app) {
 
     app.get("/api/notes", function(req, res) {
-res.json(note_data);
+        fs.readFile("./db/db.json", (err, data) => {
+            if (err) throw err;
+            noteData = JSON.parse(data);
+            res.send(noteData);
+        });
     });
 
+
     app.post("/api/notes", function(req, res) {
-        note_data.push(req.body);
-        res.json(true);
-    })
+        const userNotes = req.body;
 
-    app.delete("/api/notes/", function(req, res) {
-        note_data.length = 0;
+        fs.readFile("./db/db.json", (err, data) => {
+            if (err) throw err;
+            noteData = JSON.parse(data);
+            noteData.push(userNotes);
+            let number = 1;
+            noteData.forEach((note, index) => {
+                note.id = number;
+                number++;
+                return noteData;
+            });
+            console.log(noteData);
 
-        res.json({ok: true});
-    })
+            stringData = JSON.stringify(noteData);
+
+            fs.writeFile("./db/db.json", stringData, (err, data) => {
+                if (err) throw err;
+            });
+        });
+        res.send("Thank you for using note-taker!");
+    });
+
+    app.delete("/api/notes/:id", function(req, res) {
+
+        const deleteNote = req.params.id;
+        console.log(deleteNote);
+
+        fs.readFile("./db/db.json", (err, data) => {
+            if (err) throw err;
+
+            noteData = JSON.parse(data);
+            for (let i = 0; i < noteData.length; i++) {
+                if (noteData[i].id === Number(deleteNote)) {
+                    noteData.splice([i], 1);
+                }
+            }
+            console.log(noteData);
+            stringData = JSON.stringify(noteData);
+
+            fs.writeFile("./db/db.json", stringData, (err, data) => {
+                if (err) throw err;
+            });
+        });
+        res.status(204).send();
+    });
 };
