4787c65ee5c8bfa39ef2f4a334c5b8d539270dc2
Tonya Fletcher <tfletch4504@gmail.com>
2021-04-04 11:19:32 -0400

    server

--- routes/htmlRoute.js
+++ routes/htmlRoute.js
@@ -2,6 +2,11 @@ const path = require("path");
 module.exports = function(app) {
 
     app.get("/notes", function(req, res) {
-        res.sedFile(path.join(_dirname, ))
-    })
-}
+        res.sedFile(path.join(_dirname, "../public/assets/html/notes.html"));
+    });
+
+    app.get("*", function(req, res) {
+        res.sedFile(path.join(_dirname, "../public/assets/html/index.html"));
+    });
+
+};
--- server.js
+++ server.js
@@ -6,3 +6,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("./public/"));
 
+require("./routes/apiRoute")(app);
+require("./routes/htmlRoute")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
